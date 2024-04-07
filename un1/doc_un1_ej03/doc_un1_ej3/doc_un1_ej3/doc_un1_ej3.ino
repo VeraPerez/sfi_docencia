5edbc9abed91e6fe08396d783bb6597f31df1090
VeraPerez <111065613+VeraPerez@users.noreply.github.com>
2024-04-07 18:16:49 -0500

    Feat:develop activities tracking for Unity 1

--- un1/doc_un1_ej03/doc_un1_ej3/doc_un1_ej3/doc_un1_ej3.ino
+++ un1/doc_un1_ej03/doc_un1_ej3/doc_un1_ej3/doc_un1_ej3.ino
@@ -1,16 +1,62 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+void task1()
+{
+    // Definición de estados y variable de estado
+    enum class Task1States
+    {
+        INIT,
+        WAIT_TIMEOUT
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    // Definición de variables static (conservan su valor entre llamadas a task1)
+    static uint32_t lastTime = 0;
+
+    // Constantes
+    constexpr uint32_t INTERVAL = 1000;
+
+    // MÁQUINA de ESTADOS
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        // Acciones:
+        Serial.begin(115200);
+
+        // Garantiza los valores iniciales para el siguiente estado.
+        lastTime = millis();
+        task1State = Task1States::WAIT_TIMEOUT;
+
+        Serial.print("Task1States::WAIT_TIMEOUT\n");
+
+        break;
+    }
+    case Task1States::WAIT_TIMEOUT:
+    {
+        uint32_t currentTime = millis();
+
+        // Evento
+        if ((currentTime - lastTime) >= INTERVAL)
+        {
+            // Acciones:
+            lastTime = currentTime;
+            Serial.print(currentTime);
+            Serial.print('\n');
+        }
+        break;
+    }
+    default:
+    {
+        Serial.println("Error");
+    }
+    }
 }
 
-void loop() {
-  static uint32_t previousTime = 0;
-  static bool ledState = true;
+void setup()
+{
+    task1();
+}
 
-  uint32_t currentTime = millis();
-
-  if( (currentTime - previousTime) > 500){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+void loop()
+{
+    task1();
 }
